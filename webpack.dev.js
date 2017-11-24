99d4eee1f759f08df6d809328072c002df9f6573
olei <wzayyy2005@126.com>
2017-11-24 18:45:51 +0800

    no message

--- webpack.dev.js
+++ webpack.dev.js
@@ -59,12 +59,23 @@ const config = merge(webpackConfig, {
   ]
 })
 
+const st = webpack(config, function (err, stats) {
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n')
+  console.log(chalk.green.bold('build finish!'))
+})
 
-new WebpackDevServer(webpack(config), {
+new WebpackDevServer(st, {
     hot: true,
     compress: true,
     historyApiFallback: true,
-    publicPath: '/build/',
+    publicPath: '/',
     watchOptions: {
         ignored: /node_modules/,
     },
